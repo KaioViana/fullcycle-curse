a182c21f5099dbe09bc1d9bfcd95b5b0840b53be
kaioViana <kaiu.viana@hotmail.com>
2022-09-08 22:15:40 -0300

    add create find and findAll orders test

--- ddd-modelagem-tatica-patterns/src/infrastructure/repository/sequelize/order.repository.sequelize.spec.ts
+++ ddd-modelagem-tatica-patterns/src/infrastructure/repository/sequelize/order.repository.sequelize.spec.ts
@@ -11,12 +11,11 @@ import { ProductRepositorySequelize } from "./product.repository.sequelize";
 
 describe('Order Sequelize repository', () => {
   setupInMemorySequelize();
+  const customerRepository = new CustomerRepositorySequelize();
+  const productRepository = new ProductRepositorySequelize();
+  const orderRepository = new OrderRepositorySequelize();
 
   it('should create a new order', async () => {
-    const customerRepository = new CustomerRepositorySequelize();
-    const productRepository = new ProductRepositorySequelize();
-    const orderRepository = new OrderRepositorySequelize();
-
     const customer = new Customer('1', 'customer1')
     const address = new Address('street1', 1, 'zipcode1', 'city1');
     customer.Address = address;
@@ -52,4 +51,64 @@ describe('Order Sequelize repository', () => {
       ]
     });
   });
+
+  it('should find a order', async () => {
+    const customer = new Customer('2', 'customer1')
+    const address = new Address('street1', 1, 'zipcode1', 'city1');
+    customer.Address = address;
+    await customerRepository.create(customer);
+
+    const product = new Product('2', 'product1', 100);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem('2', product.name, product.price, product.id, 2);
+    const order = new Order('2', customer.id, [orderItem]);
+    await orderRepository.create(order);
+
+    const orderFound = await orderRepository.find(order.id);
+
+    expect(orderFound).toStrictEqual({
+      id: order.id,
+      customerId: order.customerId,
+      total: order.total(),
+      items: order.items.map(item => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        productId: item.productId,
+        quantity: item.quantity,
+        total: item.total()
+      }))
+    });
+  });
+
+  it('should find all orders', async () => {
+    const customer = new Customer('2', 'customer1')
+    const address = new Address('street1', 1, 'zipcode1', 'city1');
+    customer.Address = address;
+    await customerRepository.create(customer);
+
+    const product = new Product('2', 'product1', 100);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem('2', product.name, product.price, product.id, 2);
+    const order = new Order('2', customer.id, [orderItem]);
+    await orderRepository.create(order);
+
+    const customer2 = new Customer('3', 'customer1')
+    const address2 = new Address('street2', 1, 'zipcode1', 'city1');
+    customer2.Address = address2;
+    await customerRepository.create(customer2);
+
+    const product2 = new Product('3', 'product1', 100);
+    await productRepository.create(product2);
+
+    const orderItem2 = new OrderItem('3', product2.name, product2.price, product2.id, 2);
+    const order2 = new Order('3', customer2.id, [orderItem2]);
+    await orderRepository.create(order2);
+
+    const ordersFound = await orderRepository.findAll();
+
+    expect(ordersFound).toHaveLength(2);
+  })
 });
--- ddd-modelagem-tatica-patterns/src/infrastructure/repository/sequelize/order.repository.sequelize.ts
+++ ddd-modelagem-tatica-patterns/src/infrastructure/repository/sequelize/order.repository.sequelize.ts
@@ -3,7 +3,7 @@ import OrderRepositoryInterface from "../../../domain/repository/order-repositor
 import OrderItemModel from "../../db/sequelize/model/order-item.model";
 import OrderModel from "../../db/sequelize/model/order.model";
 
-class OrderRepositorySequelize {
+class OrderRepositorySequelize implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create({
       id: entity.id,
@@ -21,6 +21,54 @@ class OrderRepositorySequelize {
     }, {
       include: [{ model: OrderItemModel }]
     })
+  }
+
+  async find(id: string): Promise<Order> {
+    const orderFound = await OrderModel.findOne({
+      where: { id },
+      include: ["items"]
+    });
+
+    if (!orderFound) throw new Error('Order not found');
+
+    const order = {
+      id: orderFound.id,
+      customerId: orderFound.customer_id,
+      total: orderFound.total,
+      items: orderFound.items.map(item => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        productId: item.product_id,
+        quantity: item.quantity,
+        total: item.total
+      })),
+    }
+
+    return order as unknown as Order;
+  }
+
+  async findAll(): Promise<Order[]> {
+    const ordersFound = await OrderModel.findAll();
+
+    return ordersFound.map((order) => ({
+      id: order.id,
+      customerId: order.customer_id,
+      total: order.total,
+      items: order.items?.map(item => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        productId: item.product_id,
+        quantity: item.quantity,
+        total: item.total
+      })),
+    })) as unknown as Order[]
+
+  }
+
+  async update(entity: Order): Promise<void> {
+
   }
 }
 
