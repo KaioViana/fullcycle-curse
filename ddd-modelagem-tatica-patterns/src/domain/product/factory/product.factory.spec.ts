831d923e7cc2b68b0b658a470dc220a983c91784
kaioViana <kaiu.viana@hotmail.com>
2022-10-16 19:22:50 -0300

    feat: create product factory

--- ddd-modelagem-tatica-patterns/src/domain/product/factory/product.factory.spec.ts
+++ ddd-modelagem-tatica-patterns/src/domain/product/factory/product.factory.spec.ts
@@ -7,4 +7,12 @@ describe('Product factory unit test', () => {
     expect(product.name).toBe('Product A');
     expect(product.constructor.name).toBe('Product');
   });
+
+  it('should create a product type b', async () => {
+    const product = ProductFactory.create('b', 'Product B', 1);
+    expect(product.id).toBeDefined();
+    expect(product.name).toBe('Product B');
+    expect(product.constructor.name).toBe('ProductB');
+    expect(product.price).toBe(2);
+  });
 });
--- ddd-modelagem-tatica-patterns/src/domain/product/factory/product.factory.ts
+++ ddd-modelagem-tatica-patterns/src/domain/product/factory/product.factory.ts
@@ -1,9 +1,13 @@
 import Product from "../entity/product";
 import { ProductInterface } from "../entity/product.interface";
 import { v4 as uuid } from 'uuid'
+import ProductB from "../entity/product-b";
 
 class ProductFactory {
   public static create(type: string, name: string, price: number): ProductInterface {
+    if (type === 'b') {
+      return new ProductB(uuid(), name, price);
+    }
     return new Product(uuid(), name, price);
   }
 }
