e0207aec90663c9dfe10afb52f0fb500075b5e6b
kaioViana <kaiu.viana@hotmail.com>
2023-06-10 17:12:02 -0300

    sistema-moliticos

---
+++ sistemas-monoliticos/jest.config.js
@@ -0,0 +1,3 @@
+export const transform = {
+  "^.+\\.(t|j)sx?$": "@swc/jest"
+};
---
+++ sistemas-monoliticos/src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,31 @@
+import { Id } from "../value-object/id.value-object";
+
+class BaseEntity {
+  private _id: Id | undefined;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+
+  constructor(id?: Id) {
+    this._id = id;
+    this._createdAt = new Date();
+    this._updatedAt = new Date();
+  }
+
+  get id(): Id | undefined {
+    return this._id;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  get createAt(): Date {
+    return this._createdAt;
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this._updatedAt = updatedAt;
+  }
+}
+
+export { BaseEntity };
