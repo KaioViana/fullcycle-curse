6805cd3aaad47358276ad277742b58fa0ad17622
kaioViana <kaiu.viana@hotmail.com>
2023-06-14 06:45:13 -0300

    .

--- sistemas-monoliticos/src/modules/product-adm/factory/facade.factory.ts
+++ sistemas-monoliticos/src/modules/product-adm/factory/facade.factory.ts
@@ -1,4 +1,3 @@
-import { IUseCase } from "../../@shared/usecase/use-case.interface";
 import { ProductAdmFacade } from "../facade/product-adm.facade";
 import { ProductRepository } from "../repository/product.repository";
 import { AddProductUseCase } from "../usecase/add-product/add-product.usecase";
